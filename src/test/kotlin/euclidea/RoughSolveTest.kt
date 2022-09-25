fcd588db16a092c70da76fefbe28f781bb1523b0
Jonathan Edwards <jedwidz@gmail.com>
2022-09-25 16:01:22 +0800

    Solving 15.8 - partial solution

--- src/test/kotlin/euclidea/RoughSolveTest.kt
+++ src/test/kotlin/euclidea/RoughSolveTest.kt
@@ -89,6 +89,30 @@ class RoughSolveTest {
         return solutionContext!!
     }
 
+    @Test
+    fun puzzle15_8() {
+        // Line-Circle Intersection*
+        // (circles only)
+        val center = Point(0.0, 0.0)
+        val circle = Element.Circle(center, 1.0)
+        val point = Point(-2.442897, 0.0)
+        val initialContext = EuclideaContext(
+            config = EuclideaConfig(lineToolEnabled = false),
+            points = listOf(center, point),
+            elements = listOf(circle)
+        )
+        // Check for either solution point, or a useful waypoint
+        val solutionKnots = listOf(-1.0, 1.0, -2.0, 2.0)
+        fun checkSolution(a: Double, b: Double): Boolean {
+            return coincides(a, 0.0) && solutionKnots.any { coincides(it, b) }
+        }
+        // 6 - bingo (~15 sec)
+        val solutionContext = solve(initialContext, 6) { context ->
+            context.points.any { checkSolution(it.x, it.y) || checkSolution(it.y, it.x) }
+        }
+        dumpSolution(solutionContext)
+    }
+
     private fun dumpSolution(solutionContext: EuclideaContext?) {
         println(solutionContext)
         solutionContext?.let { printSteps(it) }
