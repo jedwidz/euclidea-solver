37b796f331cc6d1e5730b4c81fae43f0725fd18f
Jonathan Edwards <jedwidz@gmail.com>
2022-12-27 17:49:06 +1300

    Solving 10.4 - negative progress

--- src/test/kotlin/euclidea/SolvePuzzle10_4Test.kt
+++ src/test/kotlin/euclidea/SolvePuzzle10_4Test.kt
@@ -1,5 +1,8 @@
 package euclidea
 
+import euclidea.EuclideaTools.circleTool
+import euclidea.EuclideaTools.lineTool
+import euclidea.EuclideaTools.perpendicularTool
 import org.junit.jupiter.api.Test
 
 class SolvePuzzle10_4Test {
@@ -12,7 +15,7 @@ class SolvePuzzle10_4Test {
 
     @Test
     fun improveSolution() {
-        Solver().improveSolution(8, 8)
+        Solver().improveSolution(5, 6)
     }
 
     data class Params(
@@ -34,16 +37,16 @@ class SolvePuzzle10_4Test {
         override fun makeParams(): Params {
             return Params(
                 center = Point(0.0, 0.0),
-                point1A = Point(-0.2, -0.18),
-                point1B = Point(0.15, 0.22)
+                point1A = Point(-0.4, -0.22),
+                point1B = Point(-0.2, 0.12)
             )
         }
 
         override fun makeReplayParams(): Params {
             return Params(
                 center = Point(0.0, 0.0),
-                point1A = Point(-0.214, -0.1834),
-                point1B = Point(0.1555, 0.2211)
+                point1A = Point(-0.4142, -0.2234),
+                point1B = Point(-0.2343, 0.12124)
             )
         }
 
@@ -63,7 +66,8 @@ class SolvePuzzle10_4Test {
                 with(context) {
                     return Setup(point2A, point2B, line1, line2) to EuclideaContext(
                         config = EuclideaConfig(
-                            maxSqDistance = sq(20.0)
+                            maxSqDistance = sq(5.0),
+                            perpendicularBisectorToolEnabled = true
                         ),
                         points = listOf(center, point1A, point1B),
                         elements = listOf(line1)
@@ -78,45 +82,50 @@ class SolvePuzzle10_4Test {
         ): (EuclideaContext) -> Boolean {
             return { context ->
                 with(setup) {
-                    context.hasElement(line2)
+                    context.hasElement(line2) && context.hasPoint(point2A) && context.hasPoint(point2B)
                 }
             }
         }
 
-//        override fun pass(params: Params, setup: Setup): ((SolveContext, Element) -> Boolean) {
-//            // Euclidea L-star moves hint
-//            return { solveContext, element ->
-//                when (solveContext.depth) {
-////                    0 -> !(element.isLineFromLine && coincides(element, lineTool(params.centerA, params.centerB)))
-//                    0 -> !element.isLineFromLine
-//                    1 -> !element.isLineFromPerpendicularBisector
-//                    2 -> !element.isLineFromPerpendicularBisector
-//                    3 -> !element.isCircleFromCircle
-//                    4 -> !element.isCircleFromCircle
-//                    5 -> !element.isLineFromLine
-//                    else -> false
-//                }
-//            }
+        override fun pass(params: Params, setup: Setup): ((SolveContext, Element) -> Boolean) {
+            // Euclidea L-star moves hint
+            return { solveContext, element ->
+                when (solveContext.depth) {
+//                    0 -> !(element.isLineFromLine && coincides(element, lineTool(params.centerA, params.centerB)))
+                    0 -> !element.isLineFromLine
+                    1 -> !element.isCircleFromCircle
+                    2 -> !element.isCircleFromCircle
+                    3 -> !element.isLineFromLine
+                    4 -> !element.isCircleFromCircle
+                    5 -> !element.isLineFromPerpendicularBisector
+                    else -> false
+                }
+            }
+        }
 
-//        override fun referenceSolution(
-//            params: Params,
-//            namer: Namer
-//        ): Pair<Setup, EuclideaContext> {
-//            val (setup, initialContext) = initialContext(
-//                params, namer
-//            )
-//            with(params) {
-//                with(setup) {
-//                    // Optimal 2L solution
-//                    @Suppress("unused") val context = object {
-//                        val aim1 = perpendicularBisectorTool(point1A, point2A)
-//                        val aim2 = perpendicularBisectorTool(point1B, point2B)
-//                        val solution = intersectOnePoint(aim1, aim2)
-//                    }
-//                    namer.nameReflected(context)
-//                    return setup to initialContext.withElements(elementsReflected(context))
-//                }
-//            }
-//        }
+        override fun referenceSolution(
+            params: Params,
+            namer: Namer
+        ): Pair<Setup, EuclideaContext> {
+            val (setup, initialContext) = initialContext(
+                params, namer
+            )
+            with(params) {
+                with(setup) {
+                    // Optimal 5L solution
+                    @Suppress("unused") val context = object {
+                        val circleB = circleTool(center, point1B)
+                        val lineB = lineTool(center, point1B)
+                        val perpB = perpendicularTool(lineB, center)
+                        val solutionPointB = intersectTwoPoints(perpB, circleB).first
+                        val solution = perpendicularTool(line1, solutionPointB)
+                        val circleA = circleTool(center, point1A)
+                        val solutionPointA = intersectTwoPoints(solution, circleA).second
+                    }
+                    namer.nameReflected(context)
+                    return setup to initialContext.withElements(elementsReflected(context))
+                }
+            }
+        }
     }
 }
