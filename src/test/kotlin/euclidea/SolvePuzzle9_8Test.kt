76014dad30ec617d3044f9ad01a6cfccd78e4999
Jonathan Edwards <jedwidz@gmail.com>
2022-12-28 09:30:47 +1300

    Solving 9.8 - optimal E solution found

---
+++ src/test/kotlin/euclidea/SolvePuzzle9_8Test.kt
@@ -0,0 +1,134 @@
+package euclidea
+
+import org.junit.jupiter.api.Test
+
+class SolvePuzzle9_8Test {
+    // Harmonic Mean of Segments
+
+    @Test
+    fun checkSolution() {
+        Solver().checkReferenceSolution()
+    }
+
+    @Test
+    fun improveSolution() {
+        // solution found ~2 sec
+        Solver().improveSolution(4, 4)
+    }
+
+    data class Params(
+        val baseO: Point,
+        val baseA: Point,
+        val baseBScale: Double,
+        val dir: Point,
+        val probe: Point
+    ) {
+        val baseB = baseO + (baseA - baseO) * baseBScale
+    }
+
+    data class Setup(
+        val line1: Element.Line,
+        val line2: Element.Line
+    )
+
+    class Solver : ImprovingSolver<Params, Setup>() {
+
+        override fun makeParams(): Params {
+            return Params(
+                baseO = Point(0.0, 0.0),
+                baseA = Point(-0.3, 0.0),
+                baseBScale = -2.3,
+                dir = Point(0.5, 0.4),
+                probe = Point(0.6, 0.3)
+            )
+        }
+
+        override fun makeReplayParams(): Params {
+            return Params(
+                baseO = Point(0.0, 0.0),
+                baseA = Point(-0.7143, 0.0134),
+                baseBScale = -1.31111,
+                dir = Point(0.5012, 0.4443),
+                probe = Point(0.6001, 0.3022)
+            )
+        }
+
+        override fun initialContext(
+            params: Params,
+            namer: Namer
+        ): Pair<Setup, EuclideaContext> {
+            with(params) {
+                val context = object {
+                    // Should be a line segment - might need parameters adjusted
+                    //                    val line1 = Element.Line(baseO, baseA, limit1 = true, limit2 = true)
+                    val line1 = Element.Line(baseO, baseA)
+                    val line2 = Element.Line(baseO, dir, limit1 = true)
+                }
+                namer.nameReflected(context)
+                with(context) {
+                    return Setup(line1, line2) to EuclideaContext(
+                        config = EuclideaConfig(
+//                            perpendicularBisectorToolEnabled = true,
+//                            perpendicularToolEnabled = true,
+                            maxSqDistance = sq(20.0)
+                        ),
+                        // dir excluded
+                        points = listOf(baseO, baseA, baseB /*, probe*/),
+                        elements = listOf(line1, line2)
+                    )
+                }
+            }
+        }
+
+        override fun isSolution(
+            params: Params,
+            setup: Setup
+        ): (EuclideaContext) -> Boolean {
+            with(params) {
+                val oa = (baseA - baseO).distance
+                val ob = (baseB - baseO).distance
+                val oc = (dir - baseO).distance
+                val od = 2.0 * oa * ob / (oa + ob)
+                val checkSolutionPoint = baseO + (dir - baseO) * (od / oc)
+                return { context ->
+                    context.hasPoint(checkSolutionPoint)
+                }
+            }
+        }
+
+//        override fun pass(params: Params, setup: Setup): ((SolveContext, Element) -> Boolean) {
+//            // Euclidea 3L L-star moves hint
+//            return { solveContext, element ->
+//                when (solveContext.depth) {
+//                    0 -> element !is Element.Line
+//                    1 -> element !is Element.Line
+//                    2 -> element !is Element.Circle
+//                    else -> false
+//                }
+//            }
+//        }
+
+//        override fun referenceSolution(
+//            params: Params,
+//            namer: Namer
+//        ): Pair<Setup, EuclideaContext> {
+//            val (setup, initialContext) = initialContext(
+//                params, namer
+//            )
+//            with(params) {
+//                with(setup) {
+//                    // Optimal 3L solution
+//                    @Suppress("unused") val context = object {
+//                        val perp1 = perpendicularBisectorTool(baseA, baseB)
+//                        val perp2 = perpendicularTool(line2, baseO)
+//                        val center = intersectOnePoint(perp1, perp2)
+//                        val circle = circleTool(center, baseA)
+//                        val solution = intersectTwoPoints(circle, line2).second
+//                    }
+//                    namer.nameReflected(context)
+//                    return setup to initialContext.withElements(elementsReflected(context))
+//                }
+//            }
+//        }
+    }
+}
