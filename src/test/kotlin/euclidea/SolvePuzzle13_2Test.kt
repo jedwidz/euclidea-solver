efe93052de51a1469bec838ba89bf0c34808423f
Jonathan Edwards <jedwidz@gmail.com>
2023-01-07 17:53:13 +0800

    Solving 13.2 E - solution found

--- src/test/kotlin/euclidea/SolvePuzzle13_2Test.kt
+++ src/test/kotlin/euclidea/SolvePuzzle13_2Test.kt
@@ -18,9 +18,9 @@ class SolvePuzzle13_2Test {
 
     @Test
     fun improveSolution() {
-        // no solution found 15 sec
+        // solution found ~17 sec
         Solver().improveSolution(
-            maxExtraElements = 2,
+            maxExtraElements = 3,
             maxDepth = 8,
             nonNewElementLimit = 5,
             consecutiveNonNewElementLimit = 3,
