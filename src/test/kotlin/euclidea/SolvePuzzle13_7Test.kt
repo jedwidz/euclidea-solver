7df9f7d6d5ed275f9a1356cd183c314e6a879bdc
Jonathan Edwards <jedwidz@gmail.com>
2023-01-07 21:57:46 +0800

    Solving 13.7 E - negative progress

--- src/test/kotlin/euclidea/SolvePuzzle13_7Test.kt
+++ src/test/kotlin/euclidea/SolvePuzzle13_7Test.kt
@@ -1,5 +1,6 @@
 package euclidea
 
+import euclidea.EuclideaTools.angleBisectorTool
 import euclidea.EuclideaTools.circleTool
 import euclidea.EuclideaTools.lineTool
 import euclidea.EuclideaTools.perpendicularBisectorTool
@@ -17,9 +18,9 @@ class SolvePuzzle13_7Test {
 
     @Test
     fun improveSolution() {
-        // gave up 24 min 31 sec
+        // no solution found 36 min 16 sec
         Solver().improveSolution(
-            maxExtraElements = 3,
+            maxExtraElements = 2,
             maxDepth = 10,
             nonNewElementLimit = 6,
             consecutiveNonNewElementLimit = 4,
@@ -160,22 +161,24 @@ class SolvePuzzle13_7Test {
 //            }
 //        }
 
-//        override fun pass(params: Params, setup: Setup): ((SolveContext, Element) -> Boolean) {
-//            // Euclidea 12E E-star moves hint
-//            return { solveContext, element ->
-//                when (solveContext.depth) {
-//                    0 -> !element.isLineFromPerpendicular
-//                    1 -> !element.isCircleFromCircle
-//                    2 -> !element.isLineFromLine
-//                    3 -> !element.isLineFromPerpendicular
-//                    4 -> !element.isCircleFromCircle
-//                    5 -> !element.isCircleFromCircle
-//                    6 -> !element.isLineFromLine
-//                    7 -> !element.isLineFromLine
-//                    else -> false
-//                }
-//            }
-//        }
+        override fun pass(params: Params, setup: Setup): ((SolveContext, Element) -> Boolean) {
+            // Euclidea 10E E-star moves hint
+            return { solveContext, element ->
+                when (solveContext.depth) {
+                    0 -> !element.isCircleFromCircle
+                    1 -> !element.isCircleFromCircle
+                    2 -> !element.isLineFromLine
+                    3 -> !element.isLineFromLine
+                    4 -> !element.isCircleFromCircle
+                    5 -> !element.isCircleFromCircle
+                    6 -> !element.isLineFromLine
+                    7 -> !element.isLineFromLine
+                    8 -> !element.isLineFromLine
+                    9 -> !element.isLineFromLine
+                    else -> false
+                }
+            }
+        }
 
         override fun referenceSolution(
             params: Params,
@@ -214,36 +217,43 @@ class SolvePuzzle13_7Test {
             }
         }
 
-//        override fun additionalReferenceSolutions(): List<(Params, Namer) -> Pair<Setup, EuclideaContext?>> {
-//            return listOf(this::optimal6LSolution)
-//        }
-//
-//        private fun optimal6LSolution(
-//            params: Params,
-//            namer: Namer
-//        ): Pair<Setup, EuclideaContext> {
-//            val (setup, initialContext) = initialContext(
-//                params, namer
-//            )
-//            with(params) {
-//                with(setup) {
-//                    @Suppress("unused") val context = object {
-//                        // Optimal 6L solution
-//                        val perpC = perpendicularTool(lineCA, sample, probe = base)
-//                        val measure = circleTool(sample, center)
-//                        val down = intersectTwoPoints(measure, perpC).first
-//                        val cross = lineTool(down, base)
-//                        val aim = intersectOnePoint(cross, lineCA)
-//                        val solution1 = perpendicularTool(lineCA, aim, probe = base)
-//                        val solutionP1 = intersectOnePoint(solution1, lineBC)
-//                        val solution2 = perpendicularTool(perpC, solutionP1, probe = sample)
-//                        val solutionP2 = intersectOnePoint(solution2, lineAB)
-//                        val solution3 = perpendicularTool(lineCA, solutionP2, probe = center)
-//                    }
-//                    namer.nameReflected(context)
-//                    return setup to initialContext.withElements(elementsReflected(context))
-//                }
-//            }
-//        }
+        override fun additionalReferenceSolutions(): List<(Params, Namer) -> Pair<Setup, EuclideaContext?>> {
+            return listOf(this::optimal7LSolution)
+        }
+
+        private fun optimal7LSolution(
+            params: Params,
+            namer: Namer
+        ): Pair<Setup, EuclideaContext> {
+            val (setup, initialContext) = initialContext(
+                params, namer
+            )
+            with(params) {
+                with(setup) {
+                    @Suppress("unused") val context = object {
+                        // Optimal 7L solution
+                        val circleBack = circleTool(base, center)
+                        val circleBackInt = intersectTwoPoints(circleBack, circle)
+                        val circleBackInt1 = circleBackInt.second
+                        val circleBackInt2 = circleBackInt.first
+                        val cross = angleBisectorTool(circleBackInt1, circleBackInt2, base)
+                        val target = intersectTwoPointsOther(cross, circle, circleBackInt2)
+                        val big = circleTool(circleBackInt2, target)
+                        val opp = intersectTwoPoints(big, circleBack).first
+                        val solution1 = perpendicularBisectorTool(opp, circleBackInt1)
+                        val solutionP12 = intersectTwoPoints(solution1, circle)
+                        val solutionP1 = solutionP12.first
+                        val solutionP2 = solutionP12.second
+                        val solution2 = perpendicularTool(solution1, solutionP1, probe = center)
+                        val solutionP4 = intersectTwoPointsOther(solution2, circle, solutionP1)
+                        val solution3 = perpendicularTool(solution2, solutionP4, probe = center)
+                        val solutionP3 = intersectTwoPointsOther(solution3, circle, solutionP4)
+                        val solution4 = lineTool(solutionP3, solutionP2)
+                    }
+                    namer.nameReflected(context)
+                    return setup to initialContext.withElements(elementsReflected(context))
+                }
+            }
+        }
     }
 }
