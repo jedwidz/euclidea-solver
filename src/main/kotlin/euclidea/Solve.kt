6c5f8bcb7c6c3fe8c436c4ce062565af7869b85f
Jonathan Edwards <jedwidz@gmail.com>
2023-02-02 23:04:20 +0800

    Factor out `possibleToolApplications` for reuse

--- src/main/kotlin/euclidea/Solve.kt
+++ src/main/kotlin/euclidea/Solve.kt
@@ -27,7 +27,7 @@ private data class SolveState(
     val oldPoints: Set<Point>,
     val nonNewElementCount: Int,
     val consecutiveNonNewElementCount: Int,
-    val lastAddedElements: List<Element>,
+    val lastAddedElements: Set<Element>,
     val remainingToolSequence: List<EuclideaTool>?,
     val extraElementCount: Int,
     val unfamiliarElementCount: Int
@@ -183,104 +183,23 @@ fun solve(
             val (context, depth) = solveContext
             val nextDepth = depth + 1
             with(context) {
-                val newPoints = points.filter { it !in oldPoints }
-                val nextOldPoints = oldPoints + newPoints
-
                 val nextTool = solveState.remainingToolSequence?.first()
+                val remainingConfig =
+                    solveState.remainingToolSequence?.let { config.restrictConfig(it.toSet()) } ?: config
+                val oldElements = elements.toSet() - solveState.lastAddedElements.toSet()
 
                 val newElements = mutableSetOf<Element>()
-                fun tryAdd(e: Element?) {
+                possibleToolApplications(
+                    remainingConfig,
+                    points,
+                    oldPoints,
+                    elements,
+                    oldElements
+                ) { e: Element? ->
                     if (e !== null && e !in pendingElements && e !in passedElements && !hasElement(e) && excludeElements?.let { e in it } != true) {
                         pendingElements += e
                         if (nextTool === null || nextTool === e.sourceTool)
                             newElements += e
-                    }
-                }
-
-                val remainingConfig =
-                    solveState.remainingToolSequence?.let { config.restrictConfig(it.toSet()) } ?: config
-
-                if (remainingConfig.anyTwoPointToolEnabled) {
-                    fun visit(point1: Point, point2: Point) {
-                        if (separated(point1, point2)) {
-                            if (remainingConfig.lineToolEnabled)
-                                tryAdd(lineTool(point1, point2))
-                            if (remainingConfig.circleToolEnabled) {
-                                tryAdd(circleTool(point1, point2))
-                                tryAdd(circleTool(point2, point1))
-                            }
-                            if (remainingConfig.perpendicularBisectorToolEnabled)
-                                tryAdd(perpendicularBisectorTool(point1, point2))
-                        }
-                    }
-                    newPoints.forEachIndexed { i, newPoint ->
-                        oldPoints.forEach { visit(newPoint, it) }
-                        for (j in i + 1 until newPoints.size)
-                            visit(newPoint, newPoints[j])
-                    }
-                }
-
-                if (remainingConfig.anyLinePointToolEnabled) {
-                    fun visit(line: Element.Line, point: Point) {
-                        if (remainingConfig.perpendicularToolEnabled)
-                            tryAdd(perpendicularTool(line, point))
-                        if (remainingConfig.parallelToolEnabled)
-                            tryAdd(parallelTool(line, point))
-                    }
-
-                    val newLines = solveState.lastAddedElements.filterIsInstance<Element.Line>().toSet()
-                    val oldLines = elements.filterIsInstance<Element.Line>() - newLines
-                    oldLines.forEach { oldLine ->
-                        newPoints.forEach { newPoint ->
-                            visit(oldLine, newPoint)
-                        }
-                    }
-                    newLines.forEach { newLine ->
-                        points.forEach { point ->
-                            visit(newLine, point)
-                        }
-                    }
-                }
-
-                if (remainingConfig.anyThreePointToolEnabled) {
-                    fun visit(point1: Point, point2: Point, point3: Point) {
-                        val distance12 = distance(point1, point2)
-                        val distance23 = distance(point2, point3)
-                        val distance31 = distance(point3, point1)
-                        if (separated(distance12) && separated(distance23) && separated(distance31)) {
-                            if (remainingConfig.angleBisectorToolEnabled) {
-                                tryAdd(angleBisectorTool(point1, point2, point3))
-                                tryAdd(angleBisectorTool(point2, point3, point1))
-                                tryAdd(angleBisectorTool(point3, point1, point2))
-                            }
-                            if (remainingConfig.nonCollapsingCompassToolEnabled) {
-                                // avoids using a non-collapsing compass where a regular compass would suffice
-                                val different1 = !coincides(distance12, distance31)
-                                val different2 = !coincides(distance23, distance12)
-                                val different3 = !coincides(distance31, distance23)
-                                if (different1 && different2)
-                                    tryAdd(nonCollapsingCompassTool(point1, point2, point3))
-                                if (different2 && different3)
-                                    tryAdd(nonCollapsingCompassTool(point2, point3, point1))
-                                if (different3 && different1)
-                                    tryAdd(nonCollapsingCompassTool(point3, point1, point2))
-                            }
-                        }
-                    }
-
-                    val oldPointsList = oldPoints.toList()
-
-                    // 3 new
-                    newPoints.forEachTriple { n1, n2, n3 -> visit(n1, n2, n3) }
-
-                    // 2 new + 1 old
-                    newPoints.forEachPair { n1, n2 ->
-                        oldPointsList.forEach { o1 -> visit(n1, n2, o1) }
-                    }
-
-                    // 1 new + 2 old
-                    newPoints.forEach { n1 ->
-                        oldPointsList.forEachPair { o1, o2 -> visit(n1, o1, o2) }
                     }
                 }
 
@@ -364,6 +283,8 @@ fun solve(
                         (maxExtraElements == null || nextExtraElementCount <= maxExtraElements) &&
                         (maxUnfamiliarElements == null || nextUnfamiliarElementCount <= maxUnfamiliarElements)
                     ) {
+                        val newPoints = points.filter { it !in oldPoints }
+                        val nextOldPoints = oldPoints + newPoints
                         val nextContext = withElement(newElement)
                         val nextSolveContext = SolveContext(nextContext, nextDepth)
                         val next =
@@ -372,7 +293,7 @@ fun solve(
                                 nextOldPoints,
                                 nextNonNewElementCount,
                                 nextConsecutiveNonNewElementCount,
-                                listOf(newElement),
+                                setOf(newElement),
                                 solveState.remainingToolSequence?.drop(1),
                                 nextExtraElementCount,
                                 nextUnfamiliarElementCount
@@ -413,7 +334,7 @@ fun solve(
             oldPoints = setOf(),
             nonNewElementCount = 0,
             consecutiveNonNewElementCount = 0,
-            lastAddedElements = initialContext.elements,
+            lastAddedElements = initialContext.elements.toSet(),
             remainingToolSequence = effectiveToolSequence,
             extraElementCount = 0,
             unfamiliarElementCount = 0
@@ -425,3 +346,99 @@ fun solve(
     )
     return parallelSolver.awaitResult()
 }
+
+private fun possibleToolApplications(
+    config: EuclideaConfig,
+    allPoints: List<Point>,
+    oldPoints: Set<Point>,
+    allElements: List<Element>,
+    oldElements: Set<Element>,
+    tryAdd: (Element?) -> Unit
+) {
+    val newPoints = allPoints.filter { it !in oldPoints }
+    if (config.anyTwoPointToolEnabled) {
+        fun visit(point1: Point, point2: Point) {
+            if (separated(point1, point2)) {
+                if (config.lineToolEnabled)
+                    tryAdd(lineTool(point1, point2))
+                if (config.circleToolEnabled) {
+                    tryAdd(circleTool(point1, point2))
+                    tryAdd(circleTool(point2, point1))
+                }
+                if (config.perpendicularBisectorToolEnabled)
+                    tryAdd(perpendicularBisectorTool(point1, point2))
+            }
+        }
+        newPoints.forEachIndexed { i, newPoint ->
+            oldPoints.forEach { visit(newPoint, it) }
+            for (j in i + 1 until newPoints.size)
+                visit(newPoint, newPoints[j])
+        }
+    }
+
+    if (config.anyLinePointToolEnabled) {
+        fun visit(line: Element.Line, point: Point) {
+            if (config.perpendicularToolEnabled)
+                tryAdd(perpendicularTool(line, point))
+            if (config.parallelToolEnabled)
+                tryAdd(parallelTool(line, point))
+        }
+
+        val newElements = allElements - oldElements
+        val newLines = newElements.filterIsInstance<Element.Line>()
+        val oldLines = allElements.filterIsInstance<Element.Line>() - newLines.toSet()
+        oldLines.forEach { oldLine ->
+            newPoints.forEach { newPoint ->
+                visit(oldLine, newPoint)
+            }
+        }
+        newLines.forEach { newLine ->
+            allPoints.forEach { point ->
+                visit(newLine, point)
+            }
+        }
+    }
+
+    if (config.anyThreePointToolEnabled) {
+        fun visit(point1: Point, point2: Point, point3: Point) {
+            val distance12 = distance(point1, point2)
+            val distance23 = distance(point2, point3)
+            val distance31 = distance(point3, point1)
+            if (separated(distance12) && separated(distance23) && separated(distance31)) {
+                if (config.angleBisectorToolEnabled) {
+                    tryAdd(angleBisectorTool(point1, point2, point3))
+                    tryAdd(angleBisectorTool(point2, point3, point1))
+                    tryAdd(angleBisectorTool(point3, point1, point2))
+                }
+                if (config.nonCollapsingCompassToolEnabled) {
+                    // avoids using a non-collapsing compass where a regular compass would suffice
+                    val different1 = !coincides(distance12, distance31)
+                    val different2 = !coincides(distance23, distance12)
+                    val different3 = !coincides(distance31, distance23)
+                    if (different1 && different2)
+                        tryAdd(nonCollapsingCompassTool(point1, point2, point3))
+                    if (different2 && different3)
+                        tryAdd(nonCollapsingCompassTool(point2, point3, point1))
+                    if (different3 && different1)
+                        tryAdd(nonCollapsingCompassTool(point3, point1, point2))
+                }
+            }
+        }
+
+        val oldPointsList = oldPoints.toList()
+
+        // 3 new
+        newPoints.forEachTriple { n1, n2, n3 -> visit(n1, n2, n3) }
+
+        // 2 new + 1 old
+        newPoints.forEachPair { n1, n2 ->
+            oldPointsList.forEach { o1 -> visit(n1, n2, o1) }
+        }
+
+        // 1 new + 2 old
+        newPoints.forEach { n1 ->
+            oldPointsList.forEachPair { o1, o2 -> visit(n1, o1, o2) }
+        }
+    }
+}
+
