20f153c202e1be33dd6c40fc01d065d84244800d
Jonathan Edwards <jedwidz@gmail.com>
2023-02-04 18:55:10 +0800

    Fix bisecting solver for 'swapped bounds'

--- src/main/kotlin/euclidea/Numeric.kt
+++ src/main/kotlin/euclidea/Numeric.kt
@@ -1,17 +1,19 @@
 package euclidea
 
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sign
 
 fun solveByBisection(o1: Double, o2: Double, f: (Double) -> Double): Double {
-    val so1 = sign(f(o1))
-    val so2 = sign(f(o2))
+    var x1 = min(o1, o2)
+    var x2 = max(o1, o2)
+    val so1 = sign(f(x1))
+    val so2 = sign(f(x2))
     if (so1 == 0.0)
-        return o1
+        return x1
     if (so2 == 0.0)
-        return o2
+        return x2
     require(so1 != so2)
-    var x1 = o1
-    var x2 = o2
     while (true) {
         val m = (x1 + x2) * 0.5
         if (!(x1 < m && m < x2))
