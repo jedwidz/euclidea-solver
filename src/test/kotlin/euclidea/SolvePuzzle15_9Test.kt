a6dfedf3d89482dc4d01bea070b854904b7de87d
Jonathan Edwards <jedwidz@gmail.com>
2023-02-09 23:35:21 +0800

    Solving 15.9 E - more 'line-heavy' solution found

--- src/test/kotlin/euclidea/SolvePuzzle15_9Test.kt
+++ src/test/kotlin/euclidea/SolvePuzzle15_9Test.kt
@@ -27,7 +27,7 @@ class SolvePuzzle15_9Test {
         Solver().improveSolution(
             maxExtraElements = 6,
             maxDepth = maxDepth,
-            maxUnfamiliarElements = 0,
+            maxUnfamiliarElements = 1,
             maxNonNewElements = 5,
             maxConsecutiveNonNewElements = 3,
             maxLinesPerHeading = 2,
@@ -257,7 +257,7 @@ class SolvePuzzle15_9Test {
                 setOf(EuclideaTool.LineTool, EuclideaTool.CircleTool),
                 setOf(EuclideaTool.LineTool, EuclideaTool.CircleTool),
                 EuclideaTool.LineTool,
-                setOf(EuclideaTool.LineTool, EuclideaTool.CircleTool),
+                EuclideaTool.LineTool,
                 setOf(EuclideaTool.LineTool, EuclideaTool.CircleTool),
                 // This is skipped, just look for center of a solution
                 // EuclideaTool.CircleTool
